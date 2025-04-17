6099c797b0b0714f4d37f30d5676bcc675cc6f42
MarkSackerberg <93528482+MarkSackerberg@users.noreply.github.com>
2025-04-17 16:34:33 +0200

    update getAssetSignatures test

--- clients/js/test/getAssetSignatures.test.ts
+++ clients/js/test/getAssetSignatures.test.ts
@@ -7,33 +7,26 @@ DAS_API_ENDPOINTS.forEach((endpoint) => {
   test(`it can fetch the signatures of a compressed asset by ID (${endpoint.name})`, async (t) => {
     // Given a minted NFT on mainnet.
     const umi = createUmi(endpoint.url);
-    const assetId = publicKey('GGRbPQhwmo3dXBkJSAjMFc1QYTKGBt8qc11tTp3LkEKA');
+    const assetId = publicKey('DNzerLDcC5apcAQQ2Wx3qfRK1NPP6g5Bsg11fPpWkRtA');
 
     // When we fetch the Signatures for the asset.
     const signatures = await umi.rpc.getAssetSignatures({ assetId });
 
     // Then we expect to find 2 signatures.
-    t.is(signatures.total, 2);
+    t.is(signatures.total, 1);
     t.is(signatures.limit, 1000);
-    t.is(signatures.items.length, 2);
     t.like(signatures.items[0], <DasApiTransactionSignature>{
       signature:
-        '3Ki9QJTC2V1oNhkkhvJAJ4DEBRdMMrvKqctsyPhGL5JGkZz72xAsgTQh4vAEuJNtkxTxdmWg7RhkSXwU2JNRdSVh',
+        '2LzGEe1JZcf5SqtGhajrusvfRWSeL3Fm4sduQC1gbfPgMyQySf2tqS1yU5yTiHEutdUZkswHXHg6zzyjK6Vg92GA',
       instruction: 'MintToCollectionV1',
       slot: 356098299,
-    });
-    t.like(signatures.items[1], <DasApiTransactionSignature>{
-      signature:
-        '4rNhZukEBfuGxWxesRgStu1PqvStycu1fhNiftXVUcCygazXLpz6BubNCMywBW9eQLKtLa58FSqgyjDxHSTuFYy8',
-      instruction: 'CreateTree',
-      slot: 356098214,
     });
   });
 
   test(`it can fetch the signatures of a compressed asset by leaf index and tree (${endpoint.name})`, async (t) => {
     // Given a minted NFT on mainnet.
     const umi = createUmi(endpoint.url);
-    const tree = publicKey('J1imb8C8SPzofrtgCxkN4nsKwHevzxgvHGeYBKFEDEmE');
+    const tree = publicKey('Hw2qE4TKe8rt3VGWupLEH7wZtTrbBAzuU7LWBXfhGNMJ');
     const leaf_index = 0;
     // When we fetch the Signatures for the asset.
     const signatures = await umi.rpc.getAssetSignatures({ tree, leaf_index });
@@ -44,15 +37,9 @@ DAS_API_ENDPOINTS.forEach((endpoint) => {
     t.is(signatures.items.length, 2);
     t.like(signatures.items[0], <DasApiTransactionSignature>{
       signature:
-        '3Ki9QJTC2V1oNhkkhvJAJ4DEBRdMMrvKqctsyPhGL5JGkZz72xAsgTQh4vAEuJNtkxTxdmWg7RhkSXwU2JNRdSVh',
+        '2LzGEe1JZcf5SqtGhajrusvfRWSeL3Fm4sduQC1gbfPgMyQySf2tqS1yU5yTiHEutdUZkswHXHg6zzyjK6Vg92GA',
       instruction: 'MintToCollectionV1',
       slot: 356098299,
-    });
-    t.like(signatures.items[1], <DasApiTransactionSignature>{
-      signature:
-        '4rNhZukEBfuGxWxesRgStu1PqvStycu1fhNiftXVUcCygazXLpz6BubNCMywBW9eQLKtLa58FSqgyjDxHSTuFYy8',
-      instruction: 'CreateTree',
-      slot: 356098214,
     });
   });
 });
